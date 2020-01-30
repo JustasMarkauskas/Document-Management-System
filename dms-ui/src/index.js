5eb09e2a7d84a6e09b2fee6e9c1874bf5c11d906
giedriusgen <giedrius.genys@gmail.com>
2020-01-30 14:26:05 +0200

    AdminHomePageUsers added

--- dms-ui/src/index.js
+++ dms-ui/src/index.js
@@ -6,23 +6,29 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { Switch, Route } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 
-
-
 import LogInAdminContainer from "./components/LogInPage/LogInAdminContainer";
 import LogInUserContainer from "./components/LogInPage/LogInUserContainer";
 import AdminHomePageContainer from "./components/HomePage/AdminHomePage/AdminHomePageContainer";
 import AdminHomePageDocumentContainer from "./components/HomePage/AdminHomePage/AdminHomePageDocuments/AdminHomePageDocumentContainer";
-import AdminHomePageGroupContainer from "./components/HomePage/AdminHomePage/AdminHomePageGroups/AdminHomePageGroupsContainer"
+import AdminHomePageGroupContainer from "./components/HomePage/AdminHomePage/AdminHomePageGroups/AdminHomePageGroupsContainer";
+import AdminHomePageUsersContainer from "./components/HomePage/AdminHomePage/AdminHomePageUsers/AdminHomePageUsersContainer";
 
 ReactDOM.render(
-  <BrowserRouter>    
-      <Switch>
-        <Route exact path="/" component={LogInUserContainer} />
-        <Route path="/admin" component={LogInAdminContainer} />
-        <Route path="/adminhomepage-users" component={AdminHomePageContainer}/>
-        <Route path="/adminhomepage-documents" component={AdminHomePageDocumentContainer}/>
-        <Route path="/adminhomepage-groups" component={AdminHomePageGroupContainer}/>
-      </Switch>   
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={LogInUserContainer} />
+      <Route path="/admin" component={LogInAdminContainer} />
+      <Route path="/adminhomepage-users" component={AdminHomePageContainer} />
+      <Route
+        path="/adminhomepage-documents"
+        component={AdminHomePageDocumentContainer}
+      />
+      <Route
+        path="/adminhomepage-groups"
+        component={AdminHomePageGroupContainer}
+      />
+      <Route path="/users" component={AdminHomePageUsersContainer} />
+    </Switch>
   </BrowserRouter>,
 
   document.getElementById("root")
