f6afce376c306dd9a5af427fba4570ecc4dbf515
giedriusgen <giedrius.genys@gmail.com>
2020-02-21 14:27:11 +0200

    checkbox added

--- dms-ui/src/components/HomePage/AdminHomePage/AdminHomePageGroups/GroupReviewPage/AssignUserPage/AssignUserComponent.js
+++ dms-ui/src/components/HomePage/AdminHomePage/AdminHomePageGroups/GroupReviewPage/AssignUserPage/AssignUserComponent.js
@@ -11,69 +11,11 @@ class AssignUserComponent extends React.Component {
   render() {
     return (
       <div className="container">
-        <div className="row">
-          <div className="col-4">
-            <button
-              onClick={this.props.onOKClick}
-              type="button"
-              className="btn btn-primary m-2"
-            >
-              TE
-            </button>
-            <button
-              onClick={this.props.onOKClick}
-              type="button"
-              className="btn btn-primary m-2"
-            >
-              TEST
-            </button>
-            <button
-              onClick={this.props.onOKClick}
-              type="button"
-              className="btn btn-primary m-2"
-            >
-              TESST
-            </button>
-          </div>
-          <div className="col-7">
-            <form id="groupReviewPageId">
-              <div className="form-group">
-                <input
-                  disabled
-                  type="text"
-                  className="form-control"
-                  placeholder={this.props.groupName}
-                />
-              </div>
-              <div className="form-group">
-                <input
-                  type="text"
-                  defaultValue={this.props.comment}
-                  onChange={this.props.handleCommentChange}
-                  className="form-control"
-                  placeholder="Comment"
-                />
-              </div>
-              <div className="container mt-2">
-                <div className="row">
-                  <button
-                    onClick={this.props.onOKClick}
-                    type="button"
-                    className="btn btn-primary mr-2"
-                  >
-                    OK
-                  </button>
-                  <button
-                    onClick={this.props.onHide}
-                    type="button"
-                    className="btn btn-primary "
-                  >
-                    Cancel
-                  </button>
-                </div>
-              </div>
-            </form>
-          </div>
+        <div className="checkbox">
+          <label>
+            <input type="checkbox" value={this.props.username} />
+            {"  " + this.props.username}
+          </label>
         </div>
       </div>
     );
--- dms-ui/src/components/HomePage/AdminHomePage/AdminHomePageGroups/GroupReviewPage/AssignUserPage/AssignUserContainer.js
+++ dms-ui/src/components/HomePage/AdminHomePage/AdminHomePageGroups/GroupReviewPage/AssignUserPage/AssignUserContainer.js
@@ -12,9 +12,10 @@ class AssignUserContainer extends React.Component {
 
   getUsers = () => {
     axios
-      .get("http://localhost:8081/api/user/")
+      .get("http://localhost:8081/api/user/usernames/")
       .then(response => {
         this.setState({ users: response.data });
+        console.log(response.data);
       })
       .catch(error => {
         console.log(error);
@@ -23,43 +24,14 @@ class AssignUserContainer extends React.Component {
 
   componentDidMount() {
     this.getUsers();
-    console.log(this.state.users + "s");
   }
 
-  //   handleCommentChange = event => {
-  //     this.setState({ comment: event.target.value });
-  //   };
-
-  //   onOKClick = event => {
-  //     event.preventDefault();
-  //     axios
-  //       .put(
-  //         "http://localhost:8081/api/group/update-comment/" + this.state.group.id,
-  //         {
-  //           comment: this.state.comment,
-  //           id: "not updated"
-  //         }
-  //       )
-  //       .then(() => {})
-  //       .catch(error => {
-  //         console.log(error);
-  //       });
-  //   };
-
   render() {
-    return (
-      <AssignUserComponent
-      // groupName={this.state.group.id}
-      // comment={this.state.group.comment}
-      // groupSize={this.state.group.groupSize}
-      // groupUsers={this.state.group.groupUsers}
-      // docTypesForCreation={this.state.group.docTypesForCreation}
-      // docTypesForApproval={this.state.group.docTypesForApproval}
-      // handleCommentChange={this.handleCommentChange}
-      // onOKClick={this.onOKClick}
-      // onHide={this.props.onHide}
-      />
-    );
+    const allUsernames = this.state.users.map((user, index) => (
+      <AssignUserComponent username={user} key={index} />
+    ));
+
+    return <div>{allUsernames}</div>;
   }
 }
 
