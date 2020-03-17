119ac024be782939b30929ab975085f190479007
giedriusgen <giedrius.genys@gmail.com>
2020-03-17 09:40:10 +0200

    Password change input error msg fixed

--- dms-ui/src/components/PasswordChange/PasswodChange.js
+++ dms-ui/src/components/PasswordChange/PasswodChange.js
@@ -69,67 +69,61 @@ class PasswordChangeComponent extends React.Component {
         }}
       >
         {({ handleSubmit, handleChange, values, isValid, errors }) => (
-          <div className="NewUserForm">
+          <div className="NewUserForm password-modal-width">
             <Form noValidate onSubmit={handleSubmit}>
-              <div className="row">
-                <div className="d-flex justify-content-center col-12">
-                  <Form.Group>
-                    <Form.Control
-                      ref={this.innerRef}
-                      className="NewUserForm"
-                      size="lg"
-                      type="password"
-                      name="password"
-                      id="password"
-                      defaultValue=""
-                      onChange={handleChange}
-                      placeholder="Password"
-                      isInvalid={!!errors.password}
-                    />
-                    <Form.Control.Feedback className="FeedBack" type="invalid">
-                      <p className="text-info">{errors.password}</p>
-                    </Form.Control.Feedback>
-                  </Form.Group>
-                </div>
-                <div className="d-flex justify-content-center col-12">
-                  <Form.Group>
-                    <Form.Control
-                      className="NewUserForm"
-                      size="lg"
-                      name="confirmPassword"
-                      type="password"
-                      id="confirmPassword"
-                      defaultValue={this.state.password}
-                      onChange={this.handlePasswordChange}
-                      onKeyUp={handleChange}
-                      placeholder="Confirm password"
-                      isInvalid={!!errors.confirmPassword}
-                    />
-                    <Form.Control.Feedback className="FeedBack" type="invalid">
-                      <p className="text-info">{errors.confirmPassword}</p>
-                    </Form.Control.Feedback>
-                  </Form.Group>
-                </div>
-                <div className="d-flex justify-content-center col-12">
-                  <Button
-                    disabled={
-                      !values.password || !values.confirmPassword || !isValid
-                    }
-                    onClick={this.submitPasswordChange}
-                    variant="primary"
-                    className="SubmitButton mr-2 col-4"
-                    type="button"
-                  >
-                    Change
-                  </Button>
-                  <Button
-                    onClick={this.props.onCloseModal}
-                    variant="secondary"
-                    className="col-4"
-                  >
-                    Cancel
-                  </Button>
-                </div>
+              <Form.Group>
+                <Form.Control
+                  ref={this.innerRef}
+                  className="NewUserForm"
+                  size="lg"
+                  type="password"
+                  name="password"
+                  id="password"
+                  defaultValue=""
+                  onChange={handleChange}
+                  placeholder="Password"
+                  isInvalid={!!errors.password}
+                />
+                <Form.Control.Feedback className="FeedBack" type="invalid">
+                  <p className="text-info">{errors.password}</p>
+                </Form.Control.Feedback>
+              </Form.Group>
+              <Form.Group>
+                <Form.Control
+                  className="NewUserForm"
+                  size="lg"
+                  name="confirmPassword"
+                  type="password"
+                  id="confirmPassword"
+                  defaultValue={this.state.password}
+                  onChange={this.handlePasswordChange}
+                  onKeyUp={handleChange}
+                  placeholder="Confirm password"
+                  isInvalid={!!errors.confirmPassword}
+                />
+                <Form.Control.Feedback className="FeedBack" type="invalid">
+                  <p className="text-info">{errors.confirmPassword}</p>
+                </Form.Control.Feedback>
+              </Form.Group>
+              <div className="d-flex justify-content-center col-12">
+                <Button
+                  disabled={
+                    !values.password || !values.confirmPassword || !isValid
+                  }
+                  onClick={this.submitPasswordChange}
+                  variant="primary"
+                  className="SubmitButton mr-2 col-6"
+                  type="button"
+                >
+                  Change
+                </Button>
+                <Button
+                  onClick={this.props.onCloseModal}
+                  variant="secondary"
+                  className="col-6"
+                >
+                  Cancel
+                </Button>
               </div>
             </Form>
           </div>
